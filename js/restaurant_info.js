d6c98217a6f208e3762fffd2bed2bae025beb89a
Bill <billkariri@gmail.com>
2018-11-24 19:30:26 +0300

    feat: add alt tag to images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,6 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt = 'restaurant images'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
