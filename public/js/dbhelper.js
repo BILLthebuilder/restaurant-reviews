d00d12c6e1900c70a0321dce0f2067d54a4d484f
BILLthebuilder <billkariri@gmail.com>
2019-09-27 16:56:39 +0300

    change file directories

--- public/js/dbhelper.js
+++ public/js/dbhelper.js
@@ -8,8 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 9000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `./../data/restaurants.json`;
   }
 
   /**
@@ -156,17 +155,18 @@ class DBHelper {
   /**
    * Map marker for a restaurant.
    */
-   static mapMarkerForRestaurant(restaurant, map) {
+  static mapMarkerForRestaurant(restaurant, map) {
     // https://leafletjs.com/reference-1.3.0.html#marker  
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-      {title: restaurant.name,
-      alt: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant)
+      {
+        title: restaurant.name,
+        alt: restaurant.name,
+        url: DBHelper.urlForRestaurant(restaurant)
       })
-      marker.addTo(newMap);
+    marker.addTo(newMap);
     return marker;
-  } 
-  
+  }
+
 
 }
 
